c01c75df894e4c57d161300aa3b2e35af209ef52
GabbiePurch <purchi375@morrisville.edu>
2025-09-03 15:04:29 -0400

    Implemented Vehicle Controls
    Added movement to the vehicle.

---
+++ Assets/Scripts/VehicleController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class VehicleController : MonoBehaviour
+{
+    [SerializeField] private float movespeed = 10f;
+    [SerializeField] private float turnspeed = 700f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float move = Input.GetAxis("Vertical") * movespeed * Time.deltaTime;
+        float turn = Input.GetAxis("Horizontal") * turnspeed * Time.deltaTime;
+
+        transform.Translate(move, 0, 0);
+        transform.Rotate(0, turn, 0);
+
+        //transform.Translate(Vector3.forward * 5f * Time.deltaTime);
+        
+    }
+}
